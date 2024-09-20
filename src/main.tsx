ca67a1444c75c80f5df5071438fc31af1b63db00
TakrayoutMohamed <takrayoutm@gmail.com>
2024-09-20 17:47:12 +0100

    use it as i used to use it with react-create-app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const domNode = document.getElementById("root") as Element;
+const root = createRoot(domNode);
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
