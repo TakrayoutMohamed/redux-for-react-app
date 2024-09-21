36ea4677a34678f939700678bc0dd77eb829c77c
TakrayoutMohamed <takrayoutm@gmail.com>
2024-09-21 15:23:59 +0100

    update configuration of the typescript

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,10 +2,25 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../state/store";
 import { increment } from "../state/counter/counterSlice";
 
+/*
+* interface IActionIncrementType {
+*   type: string;
+* }
+* 
+* const actionIncrementObj: IActionIncrementType = {
+*   type: "counter/increment",
+* };
+* the commented code above is equale to the increment() function passed to dispath bellow 
+*/
+
 function Counter() {
-    const count = useSelector((state : RootState) => state.counter.value);
-    const dispatch = useDispatch();
-  return <div onClick={() => dispatch(increment())}>Counter = {count}</div>;
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+  return (
+    <div onClick={() => dispatch(increment())}>
+      Counter = {count}
+    </div>
+  );
 }
 
 export default Counter;
